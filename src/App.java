5f146349e5f96ba9f040836417466930e5191d71
Joseph DeCommer <jdecommer@gmail.com>
2024-09-24 06:43:20 -0400

    restart

--- src/App.java
+++ src/App.java
@@ -1,32 +1,30 @@
-// import algorithms.ArrayStructures;
-import algorithms.SortingAlgorithms;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-
-        //+++++++++++++++++++++++++++++++ Array Structures +++++++++++++++++++++++++++++++++++++++++++
-
-        // ArrayStructures newArray = new ArrayStructures();
-        // newArray.generateRandomArray();
         
-        // System.out.println(newArray.getValueAtIndex(9));
-        // System.out.println(newArray.doesArrayContainThisValue(9));
+        int[] arr =  {15, 83, 21, 56, 5, 7, 67, 1, 35};
 
-        // newArray.deleteIndex(3);
-        // newArray.printArray();
-
-        // newArray.insertValue(9);
-        // newArray.printArray();
-
-        // System.out.println("Linear Search");
-        // newArray.linearSearchForValue(13);
-
-        //+++++++++++++++++++++++++++++++ Sorting Algorithms +++++++++++++++++++++++++++++++++++++++++++
-
-        SortingAlgorithms sorting = new SortingAlgorithms();
-        sorting.generateRandomArray();
-
-        sorting.bubbleSort();
+        System.out.println("Before sort " +  Arrays.toString(arr));
     
+
+        System.out.println("After sort: " + bubbleSort(arr));
+
+    }
+
+    public static String bubbleSort(int[] arr) {
+
+        for(int a = 0; a < arr.length; a++) {
+
+            for(int b = 0; b <  arr.length - 1; b++) {
+
+                if(arr[b] > arr[b + 1]) {
+                    int temp = arr[a];
+                    arr[b] = arr[b + 1];
+                    arr[b + 1] = temp;
+                }
+            }
+        }
+        return Arrays.toString(arr);
     }
 }
