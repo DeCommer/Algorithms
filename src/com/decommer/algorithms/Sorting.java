0028db8399a35ff9070ef914727eaf10942afe4e
Joseph DeCommer <jdecommer@gmail.com>
2024-10-08 16:07:03 -0400

    added quicksort

--- src/com/decommer/algorithms/Sorting.java
+++ src/com/decommer/algorithms/Sorting.java
@@ -33,4 +33,32 @@ public class Sorting {
         
         return arr;
     }
+
+    public static void quickSort(int[] arr, int start, int end) {
+        if(arr != null && arr.length <= 1) {
+            return;
+        }
+        if( start < end) {
+            int pivotIndex = partition(arr, start, end);
+            quickSort(arr, start, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, end);
+        }
+    }
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int i = start;
+        for(int j = start + i; j <= end; j++) {
+            if(arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, start, i);
+        return i;
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
