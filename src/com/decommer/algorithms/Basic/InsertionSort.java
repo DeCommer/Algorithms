6a1e6b58cc469bef4b45fcada1e5bd39d9766b0d
Joseph DeCommer <jdecommer@gmail.com>
2024-10-12 12:41:04 -0400

    added file

---
+++ src/com/decommer/algorithms/Basic/InsertionSort.java
@@ -0,0 +1,11 @@
+package com.decommer.algorithms.Basic;
+
+public class InsertionSort {
+    
+
+    public static void main(String[] args) {
+        int[] array = {7, 13, 5, 1, 19, 17, 3, 15, 11, 9};
+        System.out.println("========= Insertion Sort =========");
+        
+    }
+}
--- src/com/decommer/algorithms/Basic/SelectionSort.java
+++ src/com/decommer/algorithms/Basic/SelectionSort.java
@@ -7,5 +7,39 @@ Swaps the found minimum element with the element at the current position.
  */
 
 public class SelectionSort {
+
+    public static void printArr(int[] arr) {
+        for(int n : arr) {
+            System.out.print(n + ", ");
+        }
+        System.out.println();
+    }
     
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for(int j = i + 1; j < n; j++) {
+                if(arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            if(minIdx != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] arr) {
+        int[] array = {7, 13, 5, 1, 19, 17, 3, 15, 11, 9};
+        System.out.println("========= Selection Sort =========");
+        System.out.print("Before: ");
+        printArr(array);
+        System.out.print("After : ");
+        selectionSort(array);
+        printArr(array);
+    }
+
 }
